27b7ab77e57e6be7d8a1d7ef8f5f8b47c98fda17
WorkEric <ewang@dxchain.com>
2019-07-01 14:20:22 -0700

    init

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+import "fmt"
+
+func myAdd(x int, y int) int {
+	return x + y
+}
+func testMore(){}
+
+func main() {
+	i := 5
+    fmt.Printf("hello world %v\n", i)
+}
---
+++ main_test.go
@@ -0,0 +1,11 @@
+package main
+
+import "testing"
+
+func TestAdd(t *testing.T) {
+	expected := 5
+	actual := myAdd(3, 2)
+	if (expected != actual) {
+		t.Error("Nope")
+	}
+}
