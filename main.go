943c826a1dc9110706b492f5623989c17e1f9bc1
WorkEric <ewang@dxchain.com>
2019-07-02 10:30:43 -0700

    improve coverage on test branchh

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import "fmt"
 func myAdd(x int, y int) int {
 	return x + y
 }
-func testMore(){}
 
 func main() {
 	i := 5
--- main_test.go
+++ main_test.go
@@ -5,7 +5,7 @@ import "testing"
 func TestAdd(t *testing.T) {
 	expected := 5
 	actual := myAdd(3, 2)
-	if (expected != actual) {
+	if expected != actual {
 		t.Error("Nope")
 	}
 }
