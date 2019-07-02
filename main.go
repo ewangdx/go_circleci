823794a706bcdac34fa4bc42eab5179e8c0a96e9
WorkEric <ewang@dxchain.com>
2019-07-02 10:33:13 -0700

    improve coverage again

--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
-import "fmt"
 
 func myAdd(x int, y int) int {
 	return x + y
 }
 
-func main() {
-	i := 5
-    fmt.Printf("hello world good %v\n", i)
-}
+//func main() {
+//	i := 5
+//    fmt.Printf("hello world good %v\n", i)
+//}
